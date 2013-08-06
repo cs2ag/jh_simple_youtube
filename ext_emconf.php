68a85bf4d955c0ad4e2f7fa527f09e6332155961
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-06 21:54:58 +0000

    move after wrong upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_simple_youtube/trunk@78886 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,52 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jh_simple_youtube".
+ *
+ * Auto generated 06-08-2013 23:17
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Simple Youtube',
+	'description' => 'Simply add a youtube-video as a responsive content-element. The embedded video is compatible to mobile devices (html5).',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.2.0',
+	'dependencies' => 'extbase,fluid',
+	'conflicts' => 0,
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Jonathan Heilmann',
+	'author_email' => 'mail@jonathan-heilmann.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'extbase' => '6.0',
+			'fluid' => '6.0',
+			'typo3' => '6.0.0-6.1.99',
+		),
+		'conflicts' => array(
+			'0' => '',
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:37:{s:9:"ChangeLog";s:4:"70d6";s:12:"ext_icon.gif";s:4:"8b60";s:17:"ext_localconf.php";s:4:"1e6b";s:14:"ext_tables.php";s:4:"2104";s:14:"ext_tables.sql";s:4:"d41d";s:21:"ExtensionBuilder.json";s:4:"64d5";s:10:"Readme.rst";s:4:"f13b";s:38:"Classes/Controller/VideoController.php";s:4:"7e36";s:45:"Classes/Domain/Repository/VideoRepository.php";s:4:"8351";s:44:"Configuration/ExtensionBuilder/settings.yaml";s:4:"37ad";s:41:"Configuration/FlexForms/contentPlugin.xml";s:4:"1e9f";s:38:"Configuration/TypoScript/constants.txt";s:4:"6521";s:34:"Configuration/TypoScript/setup.txt";s:4:"ae8c";s:37:"Documentation/_IncludedDirectives.rst";s:4:"53e5";s:37:"Documentation/AdministratorManual.rst";s:4:"2562";s:33:"Documentation/DeveloperCorner.rst";s:4:"e805";s:23:"Documentation/Index.rst";s:4:"a951";s:36:"Documentation/ProjectInformation.rst";s:4:"152a";s:38:"Documentation/RestructuredtextHelp.rst";s:4:"c86e";s:37:"Documentation/TyposcriptReference.rst";s:4:"5829";s:28:"Documentation/UserManual.rst";s:4:"3308";s:44:"Documentation/Images/IntroductionPackage.png";s:4:"cdeb";s:30:"Documentation/Images/Typo3.png";s:4:"4fac";s:61:"Documentation/Images/AdministratorManual/ExtensionManager.png";s:4:"14fc";s:47:"Documentation/Images/UserManual/BackendView.png";s:4:"ba6c";s:32:"Documentation/_De/UserManual.rst";s:4:"82b7";s:51:"Documentation/_De/Images/UserManual/BackendView.png";s:4:"ba6c";s:32:"Documentation/_Fr/UserManual.rst";s:4:"56b8";s:51:"Documentation/_Fr/Images/UserManual/BackendView.png";s:4:"ba6c";s:40:"Resources/Private/Language/locallang.xlf";s:4:"e930";s:54:"Resources/Private/Language/locallang_contentPlugin.xml";s:4:"6905";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"e930";s:38:"Resources/Private/Layouts/Default.html";s:4:"5117";s:43:"Resources/Private/Templates/Video/Show.html";s:4:"5d11";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:43:"Resources/Public/css/tx_jhsimpleyoutube.css";s:4:"d4a9";s:14:"doc/manual.sxw";s:4:"efc0";}',
+);
+
+?>
