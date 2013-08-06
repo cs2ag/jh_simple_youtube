6a73bcd17b86a1d0a71ceddb4260694fb61948fd
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-06 21:55:36 +0000

    move after wrong upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_simple_youtube/trunk@78889 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'Pi1',
+	'Simple Youtube'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Simple Youtube');
+
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+$pluginSignature = strtolower($extensionName) . '_pi1';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/contentPlugin.xml');
+?>
