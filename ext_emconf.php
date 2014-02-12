4aa304b7757f804147c68760d0e7d782a233c3ce
ekalinak <ekalinak@gmail.com>
2014-02-12 17:12:06 +0100

    changed dependency version of TYPO3

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'extbase' => '6.0',
 			'fluid' => '6.0',
-			'typo3' => '6.0.0-6.1.99',
+			'typo3' => '6.0.0-6.2',
 		),
 		'conflicts' => array(
 			'0' => '',
